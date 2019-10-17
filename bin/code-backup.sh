97dc9a804b8f078c54d5b2c598f4aa76afe399fa
Chuck Findlay <chuck@findlayis.me>
2019-10-17 09:38:51 -0600

    Changed location to /mnt/GoogleDrive

--- bin/code-backup.sh
+++ bin/code-backup.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 #
 # Used to regularly backup code from ~/code to a folder called Code on Google Drive in a tar file
-# Github stores this code obviously, but it's good to have backups and any secrets kept (ie, playbook var files)
-# Assumes you have google-drive-ocamlfuse setup, and that ~/GoogleDrive exists
+# Backed up seperately from other system backups done
+# Assumes you have google-drive-ocamlfuse setup, and that /mnt/GoogleDrive exists
 # Cron is your friend for it
 #
 # Author: Chuck Findlay <chuck@findlayis.me>
 # License: LGPL v3.0
 cd ~
 
-if ! grep -qs $HOME'/GoogleDrive ' /proc/mounts; then
+if ! grep -qs '/mnt/GoogleDrive ' /proc/mounts; then
     google-drive-ocamlfuse ~/GoogleDrive
 fi
 
-if grep -qs $HOME'/GoogleDrive ' /proc/mounts; then
+if grep -qs '/mnt/GoogleDrive ' /proc/mounts; then
     hostname=$(hostname)
     name=$(date '+%Y-%m-%d %H:%M')
 
     tar -zcf "/tmp/$hostname $name.tar.gz" code
-    mv "/tmp/$hostname $name.tar.gz" ~/GoogleDrive/Code
+    mv "/tmp/$hostname $name.tar.gz" /mnt/GoogleDrive/Code
 fi
